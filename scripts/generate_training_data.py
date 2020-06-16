e95308c2a2ee6478d55ce0736b90dbff48e76b7f
giovana-morais <giovana.vmorais@gmail.com>
2020-06-15 23:36:36 -0300

    change horizon and seq_len calculation

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -58,10 +58,10 @@ def generate_train_val_test(args):
     # 0 is the latest observed sample.
     x_offsets = np.sort(
         # np.concatenate(([-week_size + 1, -day_size + 1], np.arange(-11, 1, 1)))
-        np.concatenate((np.arange(-11, 1, 1),))
+        np.concatenate((np.arange(-1, 1, 1),))
     )
     # Predict the next one hour
-    y_offsets = np.sort(np.arange(1, 13, 1))
+    y_offsets = np.sort(np.arange(1, 3, 1))
     # x: (num_samples, input_length, num_nodes, input_dim)
     # y: (num_samples, output_length, num_nodes, output_dim)
     x, y = generate_graph_seq2seq_io_data(
